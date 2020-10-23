b7afc5bcf2184fd34da932cf0bddc3d9d6c42704
Pedro Martin <pataruco@gmail.com>
2020-10-23 19:28:55 +0100

    add prod server string

--- client/src/lib/get-weather.js
+++ client/src/lib/get-weather.js
@@ -1,9 +1,11 @@
 const { NODE_ENV } = process.env;
-
-console.log({ NODE_ENV });
+const serverUrl =
+  NODE_ENV === 'development'
+    ? '/forecast'
+    : 'https://young-gorge-76750.herokuapp.com/forecast';
 
 export const getWeatherData = async (location) => {
-  const response = await fetch(`/forecast?city=${location}`);
+  const response = await fetch(`${serverUrl}?city=${location}`);
   const data = await response.json();
   const { daily: weatherData } = data;
   return weatherData;
