0f56937b47c00e91ac29d5c0b9f372cc4234e7f9
Pedro Martin <pataruco@gmail.com>
2020-10-23 19:16:18 +0100

    Ready for heroku deployment

--- client/src/lib/get-weather.js
+++ client/src/lib/get-weather.js
@@ -1,3 +1,7 @@
+const { NODE_ENV } = process.env;
+
+console.log({ NODE_ENV });
+
 export const getWeatherData = async (location) => {
   const response = await fetch(`/forecast?city=${location}`);
   const data = await response.json();
