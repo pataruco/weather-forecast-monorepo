e33e2a391bacfe07e72f2ce5249142bffdaf713c
Pedro Martin <pataruco@gmail.com>
2020-10-24 20:01:23 +0100

    Add conditional server url

--- client/src/lib/get-weather.js
+++ client/src/lib/get-weather.js
@@ -1,11 +1,7 @@
-const { NODE_ENV } = process.env;
-const serverUrl =
-  NODE_ENV === 'development'
-    ? '/forecast'
-    : 'https://young-gorge-76750.herokuapp.com/forecast';
+const { REACT_APP_SERVER_URL } = process.env;
 
 export const getWeatherData = async (location) => {
-  const response = await fetch(`${serverUrl}?city=${location}`);
+  const response = await fetch(`${REACT_APP_SERVER_URL}?city=${location}`);
   const data = await response.json();
   const { daily: weatherData } = data;
   return weatherData;
